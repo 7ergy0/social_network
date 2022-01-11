bda45fe075aa9e86917739832d02fadba0d54ac6
Sergey <sergeshapluk@gmail.com>
2022-01-11 17:40:40 +0300

    RedirectWithHocTypes

--- src/hoc/withAuthRedirect.tsx
+++ src/hoc/withAuthRedirect.tsx
@@ -1,20 +1,22 @@
-import React, {Component} from "react";
+import React, {Component, ComponentType} from "react";
 import {Redirect} from "react-router-dom";
 import {RootStateType} from "../redux/redux-store";
 import {connect} from "react-redux";
 
+type mapStateToPropsForRedirectType={
+    isAuth:boolean
+}
 
-let mapStateToPropsForRedirect = (state: RootStateType) => ({
+let mapStateToPropsForRedirect = (state: RootStateType):mapStateToPropsForRedirectType => ({
     isAuth: state.auth.isAuth
 });
 
-export const withAuthRedirect=(Component:any)=>{
+export function withAuthRedirect<T>(Component:ComponentType<T>){
 
-    class RedirectComponent extends React.Component<any, any>{
-        render() {
-            if (!this.props.isAuth) return <Redirect to={'/login'}/>
-            return <Component {...this.props}/>
-        }
+    function RedirectComponent(props: mapStateToPropsForRedirectType) {
+        let {isAuth,...restProps}=props
+        if (!props.isAuth) return <Redirect to={'/login'}/>
+        return <Component {...restProps as T}/>
     }
 
     let ConnectedAuthRedirectComponent=connect(mapStateToPropsForRedirect)(RedirectComponent);
